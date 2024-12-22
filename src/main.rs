3346c2c849e8157d04f000801e87a0b50f8a9b7f
Zoey Llewellyn Hewll <zoeyhewll@gmail.com>
2024-12-22 16:40:47 +0800

    fix closure shape

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,4 @@
-fn main() {
-    fn quot(q: &str) -> String {
-        format!("\"{}\"", q.escape_default())
-    }
-    fn out(q: &str) {
-        println!("fn main () {{ {} }}", q);
-    }
-
-    (|quine_closure: &str, quot_def: &str, out_def: &str| {
+fn main () { fn quot(q: &str) -> String { format!("\"{}\"", q.escape_default()) }fn out(q: &str) { println!("fn main () {{ {} }}", q); }(|quine_closure: &str, quot_def: &str, out_def: &str| {
         let ls = [
             quot_def,
             out_def,
@@ -28,29 +20,4 @@ fn main() {
             s += st
         }
         out(&s);
-    })(
-        "|quine_closure: &str, quot_def: &str, out_def: &str| {
-        let ls = [
-            quot_def,
-            out_def,
-            quine_closure,
-            \"(\",
-            &quot(quine_closure),
-            \",\",
-            &quot(quot_def),
-            \",\",
-            &quot(out_def),
-            \",\",
-            \")\",
-        ];
-
-        let mut s = String::new();
-        for st in ls {
-            s += st
-        }
-        out(&s);
-    }",
-        "fn quot(q: &str) -> String { format!(\"\\\"{}\\\"\", q.escape_default()) }",
-        "fn out(q: &str) { println!(\"fn main () {{ {} }}\", q); }",
-    );
-}
+    })("|quine_closure: &str, quot_def: &str, out_def: &str| {\n        let ls = [\n            quot_def,\n            out_def,\n            \"(\",\n            quine_closure,\n            \")\",\n            \"(\",\n            &quot(quine_closure),\n            \",\",\n            &quot(quot_def),\n            \",\",\n            &quot(out_def),\n            \",\",\n            \")\",\n        ];\n\n        let mut s = String::new();\n        for st in ls {\n            s += st\n        }\n        out(&s);\n    }","fn quot(q: &str) -> String { format!(\"\\\"{}\\\"\", q.escape_default()) }","fn out(q: &str) { println!(\"fn main () {{ {} }}\", q); }",) }
