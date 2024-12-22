3368db203fe2b3a28728d395340ae541df7b5008
Zoey Llewellyn Hewll <zoeyhewll@gmail.com>
2024-12-22 19:33:03 +0800

    refactor: split embed function from out function

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,33 @@
+
+fn quot(s: &str) -> String {
+    format!("r#{0}{s}{0}#", '"')
+}
+fn embed(defs: &str, body: &str) -> String {
+    format!("{defs}fn main() {{{body}}}")
+}
+fn out(s: &str) {
+    println!("{s}");
+}
 fn main() {
-    fn quot(s: &str) -> String {
-        format!("r#{0}{s}{0}#", '"')
-    }
-    fn out(s: &str) {
-        println!("fn main() {{{s}}}");
-    }
-(
-    |quine_closure: &str, def: &str| {
-        out(&[
+    (|quine: &str, def: &str| {
+        out(&embed(
             def,
-            "(", quine_closure, ")(",
-                &quot(quine_closure), ",",
-                &quot(def), ",",
-            ")"
-            ].concat());
-        })(r#"
-    |quine_closure: &str, def: &str| {
-        out(&[
+            &[quine, "(", &quot(quine), ", ", &quot(def), ")"].concat(),
+        ))
+    })(r#"
+    (|quine: &str, def: &str| {
+        out(&embed(
             def,
-            "(", quine_closure, ")(",
-                &quot(quine_closure), ",",
-                &quot(def), ",",
-            ")"
-            ].concat());
-        }"#,r#"
-    fn quot(s: &str) -> String {
-        format!("r#{0}{s}{0}#", '"')
-    }
-    fn out(s: &str) {
-        println!("fn main() {{{s}}}");
-    }
-"#,)}
+            &[quine, "(", &quot(quine), ", ", &quot(def), ")"].concat(),
+        ))
+    })"#, r#"
+fn quot(s: &str) -> String {
+    format!("r#{0}{s}{0}#", '"')
+}
+fn embed(defs: &str, body: &str) -> String {
+    format!("{defs}fn main() {{{body}}}")
+}
+fn out(s: &str) {
+    println!("{s}");
+}
+"#)}
