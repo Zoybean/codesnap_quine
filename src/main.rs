0046b6f8985f7e215c43322710bc3c4ec1b351aa
Zoey Llewellyn Hewll <zoeyhewll@gmail.com>
2024-12-22 19:00:08 +0800

    use raw-string as quote fn

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
 fn main() {
-    fn quot(q: &str) -> String {
-        format!("\"{}\"", q.escape_default())
+    fn quot(s: &str) -> String {
+        format!("r#{0}{s}{0}#", '"')
     }
-    fn out(q: &str) {
-        println!("fn main() {{\n    {}\n}}", q);
+    fn out(s: &str) {
+        println!("fn main() {{{s}}}");
     }
-    (|quine_closure: &str, quot_def: &str, out_def: &str| {
-    out(&[
-        quot_def, "\n    ",
-        out_def, "\n    ",
-        "(", quine_closure, ") (\n        ",
-            &quot(quine_closure), ",\n        ",
-            &quot(quot_def), ",\n        ",
-            &quot(out_def), "\n    ",
-        ")"
-        ].concat());
-    }) (
-        "|quine_closure: &str, quot_def: &str, out_def: &str| {\n    out(&[\n        quot_def, \"\\n    \",\n        out_def, \"\\n    \",\n        \"(\", quine_closure, \") (\\n        \",\n            &quot(quine_closure), \",\\n        \",\n            &quot(quot_def), \",\\n        \",\n            &quot(out_def), \"\\n    \",\n        \")\"\n        ].concat());\n    }",
-        "fn quot(q: &str) -> String {\n        format!(\"\\\"{}\\\"\", q.escape_default())\n    }",
-        "fn out(q: &str) {\n        println!(\"fn main() {{\\n    {}\\n}}\", q);\n    }"
-    )
-}
+(
+    |quine_closure: &str, def: &str| {
+        out(&[
+            def,
+            "(", quine_closure, ")(",
+                &quot(quine_closure), ",",
+                &quot(def), ",",
+            ")"
+            ].concat());
+        })(r#"
+    |quine_closure: &str, def: &str| {
+        out(&[
+            def,
+            "(", quine_closure, ")(",
+                &quot(quine_closure), ",",
+                &quot(def), ",",
+            ")"
+            ].concat());
+        }"#,r#"
+    fn quot(s: &str) -> String {
+        format!("r#{0}{s}{0}#", '"')
+    }
+    fn out(s: &str) {
+        println!("fn main() {{{s}}}");
+    }
+"#,)}
